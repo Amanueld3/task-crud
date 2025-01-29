42a389d90acde70ec98308897b8336ca26f596e4
Aman D <amandesalegnb@gmail.com>
2025-01-29 20:11:21 +0300

    task crud

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,24 +8,22 @@ import * as bcrypt from 'bcrypt';
 export class AuthService {
   constructor(
     private jwtService: JwtService,
-    private prismaService: PrismaService, // Inject PrismaService
+    private prismaService: PrismaService,
   ) {}
 
   async validateUser({ username, password }: AuthPayloadDto) {
-    // Query the database for the user by username
     const user = await this.prismaService.user.findUnique({
       where: { username },
     });
 
     if (!user) return null;
 
-    // Assume the password is hashed, compare hashed password
     const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
 
     if (!isPasswordValid) return null;
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { hashedPassword: userPassword, ...result } = user;
-    return this.jwtService.sign(result); // Sign the JWT without the password
+    return this.jwtService.sign(result);
   }
 }
